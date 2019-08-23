54146e3521c4024f9ef6be94c46e24dbb15395ab
Nick Cipollo <nick.cipollo@thelevelup.com>
2019-08-23 11:32:54 -0400

    Print token

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import * as core from '@actions/core';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    const myInput = core.getInput('token');
+    core.debug(`Token ${myInput}`);
   } catch (error) {
     core.setFailed(error.message);
   }
