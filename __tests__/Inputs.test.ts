eed740324cdf0f4e3ed95988c0438af51ad4a9be
Nick Cipollo <njc1115@gmail.com>
2022-03-31 18:08:50 -0400

    Revert "Ensure test failures still work"
    This reverts commit 4f104f8afed5483dcfee4f7c9eb5a9c10d9d5eb7.

--- __tests__/Inputs.test.ts
+++ __tests__/Inputs.test.ts
@@ -41,7 +41,7 @@ describe('Inputs', () => {
     describe('commit', () => {
         it('returns commit', () => {
             mockGetInput.mockReturnValueOnce('commit')
-            expect(inputs.commit).toBe('commitxx')
+            expect(inputs.commit).toBe('commit')
         })
 
         it('returns undefined when omitted', () => {
